9e5c4ec33b3b57b60db3ab32d2c0e18c4a2d5756
Kalovelo <apostoloskalovelonis@gmail.com>
2021-02-08 23:41:44 +0200

    feat(newsletter): use mailchimp for newsletter subscription

--- src/components/newsletter/newsletter.tsx
+++ src/components/newsletter/newsletter.tsx
@@ -7,19 +7,40 @@ const arrowIcon = <RightOutlined style={{ fontSize: 14 }} />;
 const Newsletter = () => {
   return (
     <div className="newsletter">
-      <div className="newsletter__text">
-        <h4 className="newsletter__title">Newsletter</h4>
-        <p className="newsletter__description">
-          Μπες στη λίστα και ενημερώσου άμεσα για το επόμενο event!
-        </p>
-        <input
-          placeholder="example@domain.com"
-          type="text"
-          className="newsletter__form"
-        ></input>
+      <form
+        action={`https://gmail.us7.list-manage.com/subscribe/post?u=${process.env.MAILCHIMP_FORM_U}&amp;id=${process.env.MAILCHIMP_FORM_ID}`}
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
+      >
+        <div className="newsletter__text">
+          <h4 className="newsletter__title">Newsletter</h4>
+          <p className="newsletter__description">
+            Μπες στη λίστα και ενημερώσου άμεσα για το επόμενο event!
+          </p>
+          <input
+            placeholder="example@domain.com"
+            className="newsletter__form"
+            type="email"
+            name="EMAIL"
+            id="mce-EMAIL"
+            required
+          ></input>
 
-        <button className="button">{arrowIcon}Εγγραφή</button>
-      </div>
+          <button
+            type="submit"
+            value="Subscribe"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            className="button"
+            s
+          >
+            {arrowIcon}Εγγραφή
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
